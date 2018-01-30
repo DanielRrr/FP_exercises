8c7660e634f920a40bd0b868247f8ab25a761f67
DanielRrr <chelsea2005joecole@icloud.com>
2018-01-31 00:16:58 +0300

    added sols

--- 08/hw08/App.hs
+++ 08/hw08/App.hs
@@ -3,8 +3,8 @@ import Control.Applicative
 data Tree a = Node a [Tree a]
 
 instance Functor Tree where
-    fmap = undefined
+    fmap f (Node x xs) = Node (f x) $ map (fmap f) xs
 
 instance Applicative Tree where
-    pure = undefined
-    (<*>) = undefined
+    pure x = Node x []
+    (Node f fs) <*> (Node x xs) = Node (f x) (zipWith (<*>) fs xs)
