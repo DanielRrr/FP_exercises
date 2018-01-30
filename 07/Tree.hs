0083c7de38e8386dd000c9f724a1182f96d486f2
DanielRrr <chelsea2005joecole@icloud.com>
2018-01-31 00:07:00 +0300

    added

--- 07/Tree.hs
+++ 07/Tree.hs
@@ -6,10 +6,13 @@ data Tree a = Node { value :: a, children :: [Tree a] } deriving Eq
 
 -- show и read должны работать как описано в тестах в Main.hs
 instance Show a => Show (Tree a) where
-    show = undefined
+    show (Node x []) = show x
+    show (Node x (y:ys)) = show x ++ ":{" ++ show y ++ showL ys ++ "}:" where
+                                                    showL [] = ""
+                                                    showL (z:zs) = "," ++ show z ++ showL zs
 
 instance Read a => Read (Tree a) where
     readsPrec = undefined
 
 instance Functor Tree where
-    fmap = undefined
+    fmap f (Node x xs) = Node (f x) $ map (fmap f) xs
