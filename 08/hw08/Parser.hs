6b5607dad7abcc762cb820e6e18e3321270392d0
DanielRrr <chelsea2005joecole@icloud.com>
2018-02-06 14:01:54 +0300

    added sols

--- 08/hw08/Parser.hs
+++ 08/hw08/Parser.hs
@@ -15,7 +15,6 @@ import Data.Maybe(isNothing)
 
 newtype Parser lex a = Parser { runParser :: [lex] -> Maybe ([lex],a) }
 
--- Если парсер p не поглащает весь список, то parse должен возвращать Nothing
 parse :: Parser lex a -> [lex] -> Maybe a
 parse p xs = snd <$> runParser p xs
 
@@ -35,14 +34,23 @@ instance Functor (Parser lex) where
     fmap f = Parser . fmap (fmap (fmap f)) . runParser
 
 instance Applicative (Parser lex) where
+    -- a -> Parser lex a
     pure x = Parser $ (\lexes -> Just (lexes,x))
+    -- Parser lex (a -> b) -> Parser lex a -> Parser lex b
     Parser f <*> Parser a = Parser $ \lexs -> case f lexs of
         Nothing -> Nothing
-        Just (lexs', f') -> undefined
+        Just (lexs', f') -> case a lexs' of
+                              Nothing -> Nothing
+                              Just (lexs'', a') -> Just (lexs'', f' a')
 
 instance Alternative (Parser lex) where
-    empty = undefined
-    (<|>) = undefined
+    -- empty :: Parser lex a
+    empty = Parser $ const Nothing
+    -- (<|>) :: Parser lex a -> Parser lex a -> Parser lex a
+    Parser p1 <|> Parser p2 = Parser $ \lexes ->
+                                case p1 lexes of
+                                  Nothing -> p2 lexes
+                                  r -> r
 
 parserTestOK :: (Eq a, Show a, Eq lex, Show lex) => Parser lex a -> [lex] -> ([lex],a) -> Test
 parserTestOK (Parser p) s r = p s ~?= pure r
